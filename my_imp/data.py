ce38f357d1210ba3624b92964481da4708ac1968
Sebastian Amenabar <samenabar@uc.cl>
2019-06-24 18:25:15 -0400

    dataset v2

--- my_imp/data.py
+++ my_imp/data.py
@@ -19,20 +19,22 @@ from .vocab import Vocab
 from .copied.scene_annotation import annotate_objects
 from .copied.program_translator import clevr_to_nsclseq, nsclseq_to_nsclqsseq
 
+
 def gen_image_transform(img_size):
     return transforms.Compose([
         transforms.Resize(img_size),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
     ])
-    
+
     # return T.Compose([
     #    T.NormalizeBbox(),
     #    T.Resize(img_size),
     #    T.DenormalizeBbox(),
     #    T.ToTensor(),
     #    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    #])
+    # ])
+
 
 def gen_bbox_transform(img_size):
     transform = T.Compose([
@@ -40,12 +42,136 @@ def gen_bbox_transform(img_size):
         T.Resize(img_size),
         T.DenormalizeBbox(),
     ])
-    
+
     def fun(img, bbox):
         _, bbox = transform(img, bbox)
         return torch.from_numpy(bbox)
-        
+
     return fun
+
+
+class DatasetV2(Dataset):
+    def __init__(self,
+                 image_root,
+                 vocab_json,
+                 ans_dict_json,
+                 bbox_transform,
+                 scenes_json='',
+                 questions_json='',
+                 ds_root=None,
+                 image_transform=None,
+                 ):
+        super().__init__()
+
+        self.image_root = image_root
+        self.image_transform = image_transform
+        self.bbox_transform = bbox_transform
+
+        print('Loading vocab from: "{}".'.format(vocab_json))
+        self.vocab = Vocab.from_json(vocab_json)
+        print('Loading answers from: "{}"'.format(ans_dict_json))
+        self.ans = Vocab.from_json(ans_dict_json)
+
+        cached_scenes = osp.join(
+            ds_root, 'scenes_cache.pkl') if ds_root else ''
+        if cached_scenes and osp.exists(cached_scenes):
+            print('==> using cached scenes: {}'.format(cached_scenes))
+            with open(cached_scenes, 'rb') as f:
+                self.scenes = pickle.load(f)
+        else:
+            with open(scenes_json, 'r') as f:
+                self.scenes = json.load(f)
+            self.prepare_scenes()
+            with open(cached_scenes, 'wb') as f:
+                pickle.dump(self.scenes, f)
+
+        cached_questions = osp.join(
+            ds_root, 'questions_cache.pkl') if ds_root else ''
+        if cached_questions and osp.exists(cached_questions):
+            print('==> using cached questions: {}'.format(cached_questions))
+            with open(cached_questions, 'rb') as f:
+                self.questions = pickle.load(f)
+        else:
+            with open(questions_json, 'r') as f:
+                self.questions = json.load(f)
+            self.prepare_questions()
+            with open(cached_questions, 'wb') as f:
+                pickle.dump(self.questions, f)
+
+    def prepare_scenes(self):
+        print('Preparing scenes')
+        if type(self.scenes) is list:
+            dummy_fp = osp.join(
+                self.image_root, self.scenes[0]['image_filename'])
+            scene_iter = self.scenes
+        elif type(self.scenes) is dict:
+            self.scenes = {int(key): val for key, val in self.scenes.items()}
+            dummy_fp = osp.join(self.image_root, list(
+                self.scenes.values())[0]['image_filename'])
+            scene_iter = self.scenes.values()
+        else:
+            raise Exception(
+                f"Scenes type is '{type(self.scenes)}', expeceted 'list' or 'dict'")
+        dummy_image = Image.open(dummy_fp).convert('RGB')
+        for i, scene in enumerate(scene_iter):
+            # scene = scenes['scenes'][i]
+            print(f'\r{i + 1}/{len(self.scenes)}', end='')
+            objects = annotate_objects(scene)['objects']
+            # scene['objects_raw'] = scene['objects']
+            scene['objects'] = self.bbox_transform(dummy_image, objects)
+            scene['scene_size'] = len(scene['objects'])
+            scene['image_filename'] = osp.join(
+                self.image_root, scene['image_filename'])
+
+            del scene['relationships']
+            del scene['objects_detection']
+            del scene['directions']
+        print()
+
+    def prepare_questions(self):
+        print('\nPreparing questions')
+        for i, q in enumerate(self.questions):
+            print(f'\r{i + 1}/{len(self.questions)}', end='')
+            q['program_size'] = len(q['program'])
+            q['question_raw'] = q['question']
+            q['question'] = np.array(self.vocab.map_sequence(
+                nltk.word_tokenize(q['question'].lower())), dtype='int32')
+            q['answer_raw'] = q['answer']
+            q['answer'] = self.ans.word2idx[q['answer']]
+            # q['program_raw'] = q['program']
+            # q['program_seq'] = clevr_to_nsclseq(q['program'])
+            program_seq = clevr_to_nsclseq(q['program'])
+            q['program_qsseq'] = nsclseq_to_nsclqsseq(program_seq)
+            q['question_type'] = program_seq[-1]['op']
+
+            del q['program']
+            del q['image_filename']
+
+        print()
+
+    def __getitem__(self, index):
+        # fd = self.questions[index]
+        # scene = self.scenes[fd['image_index']]
+
+        # image = Image.open(scene['image_filename']).convert('RGB')
+        # image = self.image_transform(image)
+
+        # Testing without vars because of memory leaks
+        return {
+            'image': self.image_transform(Image.open(self.scenes[self.questions[index]['image_index']]['image_filename']).convert('RGB')),
+            **self.questions[index],
+            'objects': self.scenes[self.questions[index]['image_index']]['objects'],
+        }
+
+    def _get_metainfo(self, index):
+        return {
+            'question': self.questions[index],
+            'scene': self.scenes[self.questions[index]['image_index']],
+        }
+
+    def __len__(self):
+        return len(self.questions)
+
 
 class MyDataset(Dataset):
     def __init__(self, scenes_json, questions_json,
@@ -90,8 +216,10 @@ class MyDataset(Dataset):
         if sample_size:
             if seed:
                 np.random.seed(seed)
-            self.questions = np.random.choice(self.questions, size=sample_size, replace=False)
-            self.scenes = {q['image_index']: self.scenes[q['image_index']] for q in self.questions}
+            self.questions = np.random.choice(
+                self.questions, size=sample_size, replace=False)
+            self.scenes = {q['image_index']: self.scenes[q['image_index']]
+                           for q in self.questions}
 
         # print(len(self.questions))
         # print(len(self.scenes))
@@ -105,7 +233,8 @@ class MyDataset(Dataset):
     def prepare_data(self):
 
         print('Preparing scenes')
-        dummy_fp = osp.join(self.image_root, self.scenes[self.questions[0]['image_index']]['image_filename'])
+        dummy_fp = osp.join(
+            self.image_root, self.scenes[self.questions[0]['image_index']]['image_filename'])
         dummy_image = Image.open(dummy_fp).convert('RGB')
         if type(self.scenes) is list:
             scene_iter = self.scenes
@@ -118,7 +247,8 @@ class MyDataset(Dataset):
             # scene['objects_raw'] = scene['objects']
             scene['objects'] = self.bbox_tranform(dummy_image, objects)
             scene['scene_size'] = len(scene['objects'])
-            scene['image_filename'] = osp.join(self.image_root, scene['image_filename'])
+            scene['image_filename'] = osp.join(
+                self.image_root, scene['image_filename'])
 
             del scene['relationships']
             del scene['objects_detection']
@@ -131,7 +261,8 @@ class MyDataset(Dataset):
             print(f'\r{i + 1}/{len(self.questions)}', end='')
             q['program_size'] = len(q['program'])
             q['question_raw'] = q['question']
-            q['question'] = np.array(self.vocab.map_sequence(nltk.word_tokenize(q['question'].lower())), dtype='int32')
+            q['question'] = np.array(self.vocab.map_sequence(
+                nltk.word_tokenize(q['question'].lower())), dtype='int32')
             q['answer_raw'] = q['answer']
             q['answer'] = self.ans.word2idx[q['answer']]
             # q['program_raw'] = q['program']
@@ -158,24 +289,33 @@ class MyDataset(Dataset):
             'image': self.image_transform(Image.open(self.scenes[self.questions[index]['image_index']]['image_filename']).convert('RGB')),
             **self.questions[index],
             'objects': self.scenes[self.questions[index]['image_index']]['objects'],
-            }
+        }
+
+    def _get_metainfo(self, index):
+        return {
+            'question': self.questions[index],
+            'scene': self.scenes[self.questions[index]['image_index']],
+        }
 
     def __len__(self):
         return len(self.questions)
+
 
 def collate_fn(batch):
     # batch_size = len(batch)
 
     images = torch.stack([d['image'] for d in batch])
     answers = torch.tensor([d['answer'] for d in batch], dtype=torch.long)
-    objects_len = torch.tensor([len(d['objects']) for d in batch], dtype=torch.uint8)
+    objects_len = torch.tensor([len(d['objects'])
+                                for d in batch], dtype=torch.uint8)
     objects = pad_sequence([d['objects'] for d in batch], batch_first=True)
-    questions = pad_sequence([torch.tensor(d['question'], dtype=torch.long) for d in batch], batch_first=True)
+    questions = pad_sequence(
+        [torch.tensor(d['question'], dtype=torch.long) for d in batch], batch_first=True)
     # programs = AttrDict({
     #    'program_raw': [d['program_raw'] for d in batch],
     #    'program_seq': [d['program_seq'] for d in batch],
     #    'program_qsseq': [d['program_qsseq'] for d in batch],
-    #})
+    # })
     program_qsseq = [d['program_qsseq'] for d in batch]
 
     return AttrDict({
