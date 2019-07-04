17eda8dfae58e073db55ad8b2497732fc96a576e
Sebastian Amenabar <samenabar@uc.cl>
2019-07-04 15:30:09 -0400

    modified mac

---
+++ pytorch-mac-network/code/datasets.py
@@ -0,0 +1,129 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import glob
+import json
+import os
+import os.path
+import pickle
+import random
+import re
+from pathlib import Path
+
+import h5py
+import numpy as np
+import PIL
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import pycocotools.mask as mask_utils
+from PIL import Image
+
+from config import cfg
+
+initial_size = (320, 480)
+final_size = (149, 224)
+
+_transform = transforms.Compose([
+    transforms.Resize(final_size),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+def norm_bbox(bbox, initial_size=initial_size, final_size=final_size):
+    bbox = bbox.copy()
+    bbox[:, 0] = (bbox[:, 0] / initial_size[1]) * final_size[1]
+    bbox[:, 1] = (bbox[:, 1] / initial_size[0]) * final_size[0]
+    bbox[:, 2] = (bbox[:, 2] / initial_size[1]) * final_size[1]
+    bbox[:, 3] = (bbox[:, 3] / initial_size[0]) * final_size[0]
+
+    return bbox.astype(np.int)
+
+class ClevrDataset(data.Dataset):
+    def __init__(self, data_dir, img_dir, scenes_json, split='train'):
+
+        with open(os.path.join(data_dir, '{}.pkl'.format(split)), 'rb') as f:
+            self.data = pickle.load(f)
+        
+        fp_data = os.path.join(data_dir, '{}_features.h5'.format(split))
+        if os.path.exists(fp_data):
+            self.img = h5py.File(fp_data, 'r')['features']
+        else:
+            self.img = h5py.File(os.path.join(
+                data_dir, '{}.h5'.format(split)), 'r')['features']
+        
+
+        with open(scenes_json, 'r') as f:
+            self.scenes = json.load(f)['scenes']
+        self.imgfile2idx = {
+            scene['image_filename']: i for i, scene in enumerate(self.scenes)}
+        
+        self.img_dir = img_dir
+        self.split = split
+
+        self.prepare_scenes()
+
+    def prepare_scenes(self):
+        print('Preparing scenes')
+        for i, scene in enumerate(self.scenes):
+            print(f'\r{i + 1}/{len(self.scenes)}', end='')
+            # print(scene['objects_detection'])
+            boxes = np.array([mask_utils.toBbox(obj['mask'])
+                               for obj in scene['objects_detection']])
+            scene['boxes'] = norm_bbox(boxes)
+        print()
+
+    def __getitem__(self, index):
+        imgfile, question, answer, family = self.data[index]
+        scene = self.scenes[self.imgfile2idx[imgfile]]
+
+        assert scene['image_filename'] == imgfile
+
+        id = int(imgfile.rsplit('_', 1)[1][:-4])
+        img = torch.from_numpy(self.img[id])
+
+        raw_image = Image.open(os.path.join(self.img_dir, self.split, imgfile)).convert('RGB')
+        raw_image = _transform(raw_image)
+
+        return img, question, len(question), answer, family, torch.from_numpy(scene['boxes']), raw_image
+
+    def __len__(self):
+        return len(self.data)
+
+
+def collate_fn(batch):
+    images, lengths, answers, boxes, raw_images = [], [], [], [], []
+    batch_size = len(batch)
+
+    max_len = max(map(lambda x: len(x[1]), batch))
+    max_objects_len = max(map(lambda x: len(x[5]), batch))
+
+    questions = np.zeros((batch_size, max_len), dtype=np.int64)
+    # objects =
+
+    sort_by_len = sorted(batch, key=lambda x: len(x[1]), reverse=True)
+
+    for i, b in enumerate(sort_by_len):
+        image, question, length, answer, family, box, raw_image = b
+        images.append(image)
+        length = len(question)
+        questions[i, :length] = question
+        lengths.append(length)
+        answers.append(answer)
+        boxes.append(box)
+        raw_images.append(raw_image)
+
+    raw_images = torch.stack(raw_images)
+    raw_images = raw_images.unsqueeze(1)
+    raw_images = raw_images.repeat(1, max_objects_len, 2, 1, 1)
+    raw_images[:, :, 3:, :, :] = 0
+
+    for img_number, image_boxes in enumerate(boxes):
+        for obj_number, box in enumerate(image_boxes):
+            raw_images[img_number, obj_number, 3:, box[1]:box[1]+box[3], box[0]:box[0]+box[2]] = raw_images[img_number, obj_number, :3, box[1]:box[1]+box[3], box[0]:box[0]+box[2]]
+
+
+    return {'image': torch.stack(images), 'question': torch.from_numpy(questions),
+            'answer': torch.LongTensor(answers), 'question_length': lengths,
+            'objects': raw_images,
+            }
---
+++ pytorch-mac-network/code/mac.py
@@ -0,0 +1,333 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+from torch.autograd import Variable
+from torchvision.models import resnet34
+
+from utils import *
+
+
+def load_MAC(cfg, vocab):
+    kwargs = {'vocab': vocab,
+              'max_step': cfg.TRAIN.MAX_STEPS
+              }
+
+    model = MACNetwork(cfg, **kwargs)
+    model_ema = MACNetwork(cfg, **kwargs)
+    for param in model_ema.parameters():
+        param.requires_grad = False
+
+    if torch.cuda.is_available():
+        model.cuda()
+        model_ema.cuda()
+    else:
+        model.cpu()
+        model_ema.cpu()
+    model.train()
+    return model, model_ema
+
+
+class ControlUnit(nn.Module):
+    def __init__(self, cfg, module_dim, max_step=4):
+        super().__init__()
+        self.cfg = cfg
+        self.attn = nn.Linear(module_dim, 1)
+        self.control_input = nn.Sequential(nn.Linear(module_dim, module_dim),
+                                           nn.Tanh())
+
+        self.control_input_u = nn.ModuleList()
+        for i in range(max_step):
+            self.control_input_u.append(nn.Linear(module_dim, module_dim))
+
+        self.module_dim = module_dim
+
+    def mask(self, question_lengths, device):
+        max_len = question_lengths.max().item()
+        mask = torch.arange(max_len, device=device).expand(len(question_lengths), int(max_len)) < question_lengths.unsqueeze(1)
+        mask = mask.float()
+        ones = torch.ones_like(mask)
+        mask = (ones - mask) * (1e-30)
+        return mask
+
+    def forward(self, question, context, question_lengths, step):
+        """
+        Args:
+            question: external inputs to control unit (the question vector).
+                [batchSize, ctrlDim]
+            context: the representation of the words used to compute the attention.
+                [batchSize, questionLength, ctrlDim]
+            control: previous control state
+            question_lengths: the length of each question.
+                [batchSize]
+            step: which step in the reasoning chain
+        """
+        # compute interactions with question words
+        question = self.control_input(question)
+        question = self.control_input_u[step](question)
+
+        newContControl = question
+        newContControl = torch.unsqueeze(newContControl, 1)
+        interactions = newContControl * context
+
+        # compute attention distribution over words and summarize them accordingly
+        logits = self.attn(interactions)
+
+        # TODO: add mask again?!
+        # question_lengths = torch.cuda.FloatTensor(question_lengths)
+        # mask = self.mask(question_lengths, logits.device).unsqueeze(-1)
+        # logits += mask
+        attn = F.softmax(logits, 1)
+
+        # apply soft attention to current context words
+        next_control = (attn * context).sum(1)
+
+        return next_control
+
+
+class ReadUnit(nn.Module):
+    def __init__(self, module_dim):
+        super().__init__()
+
+        self.concat = nn.Linear(module_dim * 2, module_dim)
+        self.concat_2 = nn.Linear(module_dim, module_dim)
+        self.attn = nn.Linear(module_dim, 1)
+        self.dropout = nn.Dropout(0.15)
+        self.kproj = nn.Linear(module_dim, module_dim)
+        self.mproj = nn.Linear(module_dim, module_dim)
+
+        self.activation = nn.ELU()
+        self.module_dim = module_dim
+
+    def forward(self, memory, know, control, objects=None, memDpMask=None):
+        """
+        Args:
+            memory: the cell's memory state
+                [batchSize, memDim]
+
+            know: representation of the knowledge base (image).
+                [batchSize, kbSize (Height * Width), memDim]
+
+            control: the cell's control state
+                [batchSize, ctrlDim]
+
+            memDpMask: variational dropout mask (if used)
+                [batchSize, memDim]
+        """
+        ## Step 1: knowledge base / memory interactions
+        # compute interactions between knowledge base and memory
+        know = self.dropout(know)
+
+        # print(1, know.size())
+        if memDpMask is not None:
+            if self.training:
+                memory = applyVarDpMask(memory, memDpMask, 0.85)
+        else:
+            memory = self.dropout(memory)
+        know_proj = self.kproj(know)
+        # print(2, know_proj.size())
+        memory_proj = self.mproj(memory)
+        memory_proj = memory_proj.unsqueeze(1)
+        interactions = know_proj * memory_proj
+        # print(3, interactions.size())
+
+        # project memory interactions back to hidden dimension
+        interactions = torch.cat([interactions, know_proj], -1)
+        interactions = self.concat(interactions)
+        interactions = self.activation(interactions)
+        interactions = self.concat_2(interactions)
+
+        # print(4, interactions.size())
+
+        ## Step 2: compute interactions with control
+        control = control.unsqueeze(1)
+        interactions = interactions * control
+        interactions = self.activation(interactions)
+
+        # print(5, interactions.size())
+
+        ## Step 3: sum attentions up over the knowledge base
+        # transform vectors to attention distribution
+        interactions = self.dropout(interactions)
+        attn = self.attn(interactions).squeeze(-1)
+        attn = F.softmax(attn, 1)
+
+        # sum up the knowledge base according to the distribution
+        attn = attn.unsqueeze(-1)
+        read = (attn * know).sum(1)
+
+        return read
+
+
+class WriteUnit(nn.Module):
+    def __init__(self, cfg, module_dim):
+        super().__init__()
+        self.cfg = cfg
+        self.linear = nn.Linear(module_dim * 2, module_dim)
+
+    def forward(self, memory, info):
+        newMemory = torch.cat([memory, info], -1)
+        newMemory = self.linear(newMemory)
+
+        return newMemory
+
+
+class MACUnit(nn.Module):
+    def __init__(self, cfg, module_dim=512, max_step=4):
+        super().__init__()
+        self.cfg = cfg
+        self.control = ControlUnit(cfg, module_dim, max_step)
+        self.read = ReadUnit(module_dim)
+        self.write = WriteUnit(cfg, module_dim)
+
+        self.initial_memory = nn.Parameter(torch.zeros(1, module_dim))
+
+        self.module_dim = module_dim
+        self.max_step = max_step
+
+    def zero_state(self, batch_size, question):
+        initial_memory = self.initial_memory.expand(batch_size, self.module_dim)
+        initial_control = question
+
+        if self.cfg.TRAIN.VAR_DROPOUT:
+            memDpMask = generateVarDpMask((batch_size, self.module_dim), 0.85)
+        else:
+            memDpMask = None
+
+        return initial_control, initial_memory, memDpMask
+
+    def forward(self, context, question, knowledge, question_lengths, objects=None):
+        batch_size = question.size(0)
+        control, memory, memDpMask = self.zero_state(batch_size, question)
+
+        for i in range(self.max_step):
+            # control unit
+            control = self.control(question, context, question_lengths, i)
+            # read unit
+            info = self.read(memory, knowledge, control, objects, memDpMask)
+            # write unit
+            memory = self.write(memory, info)
+
+        return memory
+
+
+class InputUnit(nn.Module):
+    def __init__(self, cfg, vocab_size, wordvec_dim=300, rnn_dim=512, module_dim=512, bidirectional=True):
+        super(InputUnit, self).__init__()
+
+        self.dim = module_dim
+        self.cfg = cfg
+
+        self.stem = nn.Sequential(nn.Dropout(p=0.18),
+                                  nn.Conv2d(1024, module_dim, 3, 1, 1),
+                                  nn.ELU(),
+                                  nn.Dropout(p=0.18),
+                                  nn.Conv2d(module_dim, module_dim, kernel_size=3, stride=1, padding=1),
+                                  nn.ELU())
+
+        self.bidirectional = bidirectional
+        if bidirectional:
+            rnn_dim = rnn_dim // 2
+
+        self.encoder_embed = nn.Embedding(vocab_size, wordvec_dim)
+        self.encoder = nn.LSTM(wordvec_dim, rnn_dim, batch_first=True, bidirectional=bidirectional)
+        self.embedding_dropout = nn.Dropout(p=0.15)
+        self.question_dropout = nn.Dropout(p=0.08)
+
+    def forward(self, image, question, question_len):
+        b_size = question.size(0)
+
+        # get image features
+        img = self.stem(image)
+        img = img.view(b_size, self.dim, -1)
+        img = img.permute(0,2,1)
+
+        # get question and contextual word embeddings
+        embed = self.encoder_embed(question)
+        embed = self.embedding_dropout(embed)
+        embed = nn.utils.rnn.pack_padded_sequence(embed, question_len, batch_first=True)
+
+        contextual_words, (question_embedding, _) = self.encoder(embed)
+        if self.bidirectional:
+            question_embedding = torch.cat([question_embedding[0], question_embedding[1]], -1)
+        question_embedding = self.question_dropout(question_embedding)
+
+        contextual_words, _ = nn.utils.rnn.pad_packed_sequence(contextual_words, batch_first=True)
+
+        return question_embedding, contextual_words, img
+
+
+class OutputUnit(nn.Module):
+    def __init__(self, module_dim=512, num_answers=28):
+        super(OutputUnit, self).__init__()
+
+        self.question_proj = nn.Linear(module_dim, module_dim)
+
+        self.classifier = nn.Sequential(nn.Dropout(0.15),
+                                        nn.Linear(module_dim * 2, module_dim),
+                                        nn.ELU(),
+                                        nn.Dropout(0.15),
+                                        nn.Linear(module_dim, num_answers))
+
+    def forward(self, question_embedding, memory):
+        # apply classifier to output of MacCell and the question
+        question_embedding = self.question_proj(question_embedding)
+        out = torch.cat([memory, question_embedding], 1)
+        out = self.classifier(out)
+
+        return out
+
+
+class MACNetwork(nn.Module):
+    def __init__(self, cfg, max_step, vocab, recv_objects=False):
+        super().__init__()
+
+        self.cfg = cfg
+        encoder_vocab_size = len(vocab['question_token_to_idx'])
+
+        self.input_unit = InputUnit(cfg, vocab_size=encoder_vocab_size)
+
+        self.output_unit = OutputUnit()
+
+        self.mac = MACUnit(cfg, max_step=max_step)
+
+        init_modules(self.modules(), w_init=self.cfg.TRAIN.WEIGHT_INIT)
+        nn.init.uniform_(self.input_unit.encoder_embed.weight, -1.0, 1.0)
+        nn.init.normal_(self.mac.initial_memory)
+
+        self.recv_objects = recv_objects
+        if recv_objects:
+            resnet = resnet34(pretrained=True)
+            self.obj_linear = nn.Linear(512, 512)
+            
+            
+            self.obj_layers = list(resnet.children())
+            self.obj_layers.pop()
+            self.obj_layers.pop(0)
+            self.obj_layers.insert(0, nn.Conv2d(6, 64, kernel_size=(7, 7),
+                                    stride=(2, 2), padding=(3, 3), bias=False))
+            self.obj_layers = nn.Sequential(*self.obj_layers)
+
+    def forward(self, image, question, question_len, objects=None):
+        # get image, word, and sentence embeddings
+        question_embedding, contextual_words, img = self.input_unit(image, question, question_len)
+
+        if self.recv_objects:
+            bsz, objs_len = objects.size()[:2]
+            objects = self.obj_layers(objects.view(bsz * objs_len, 6, 149, 224))
+            objects = self.obj_linear(objects.squeeze(-1).squeeze(-1))
+            objects = objects.view(bsz, objs_len, -1)
+            # print(objects.size())
+
+            # img = torch.cat((img, objects), dim=1)
+            img = objects
+            # print(img.size())
+
+        # return 
+
+        # apply MacCell
+        memory = self.mac(contextual_words, question_embedding, img, question_len, objects)
+
+        # get classification
+        out = self.output_unit(question_embedding, memory)
+
+        return out
