353ca5dfd0ae24e3b907e1f22ffefa21053d3ed7
root <samenabar@uc.cl>
2019-07-07 19:59:42 +0000

    some fixes

--- pytorch-mac-network/code/mac.py
+++ pytorch-mac-network/code/mac.py
@@ -300,7 +300,7 @@ class MACNetwork(nn.Module):
         nn.init.uniform_(self.input_unit.encoder_embed.weight, -1.0, 1.0)
         nn.init.normal_(self.mac.initial_memory)
 
-        self.input_unit.fpn = resnet_fpn_backbone('resnet101', pretrained=True)
+        self.input_unit.fpn = resnet_fpn_backbone('resnet50', pretrained=True)
         # No actualizar pesos de resnet
         for param in self.input_unit.fpn.body.parameters():
             param.requires_grad = False
@@ -325,6 +325,7 @@ class MACNetwork(nn.Module):
             j = 0
             for i in bboxes_len:
                 ts.append(rois[j:j+i])
+                j += i
 
             objects = pad_sequence(ts, batch_first=True)
 
